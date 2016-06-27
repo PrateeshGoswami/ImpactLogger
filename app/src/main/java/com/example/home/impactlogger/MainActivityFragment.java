bae49b230dac3a011faac973a2aa3dad0223eb3f
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-06-27 10:18:16 -0700

    Feat:Add Scan button

--- app/src/main/java/com/example/home/impactlogger/MainActivityFragment.java
+++ app/src/main/java/com/example/home/impactlogger/MainActivityFragment.java
@@ -2,7 +2,10 @@ package com.example.home.impactlogger;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +16,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
@@ -21,64 +25,121 @@ import java.util.Set;
 public class MainActivityFragment extends Fragment {
 
     ArrayAdapter<String> mDeviceAdapter;
-    Button connectNew;
+    Button scanButton;
     ListView listView;
     BluetoothAdapter btAdapter;
     Set<BluetoothDevice> devicesArray;
+    ArrayList<String> pairedDevices;
+    IntentFilter filter;
+    BroadcastReceiver receiver;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-//        String[] deviceArray = {
-//                "John",
-//                "Max",
-//                "Kevin",
-//                "Yash"
-//        };
-//        List<String> deviceList = new ArrayList<String>(Arrays.asList(deviceArray));
-//        //ArrayAdapter
-//        mDeviceAdapter = new ArrayAdapter<String>(
-//                getActivity(),
-//                R.layout.list_item_device,
-//                R.id.list_item_device_textview,
-//                deviceArray
-//        );
-//        //get refernce to the listview and attach this adapter
-//        listView = (ListView)rootView.findViewById(
-//                R.id.listview_devices);
-//        listView.setAdapter(mDeviceAdapter);
-        connectNew = (Button) rootView.findViewById(R.id.bConnectionNew);
+
         listView = (ListView) rootView.findViewById(R.id.listview_devices);
+        scanButton = (Button) rootView.findViewById(R.id.bConnectionNew);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDiscovery();
+            }
+        });
+
         mDeviceAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 R.layout.list_item_device,
                 R.id.list_item_device_textview);
         listView.setAdapter(mDeviceAdapter);
+      
+
         btAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter==null){
-            Toast.makeText(getActivity(),"No Bluetooth Detected",Toast.LENGTH_LONG).show();
-               }else {
-            if(!btAdapter.isEnabled()){
-                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(intent,1);
+        pairedDevices = new ArrayList<String>();
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    mDeviceAdapter.add(device.getName() + "\n" + device.getAddress());
+                } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+
+                } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+
+                } else if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+                    if (btAdapter.getState() == btAdapter.STATE_OFF) {
+                        turnOnBt();
+
+                    }
+
+                }
+
+            }
+
+        };
+
+        if (btAdapter == null) {
+            Toast.makeText(getActivity(), "No Bluetooth Detected", Toast.LENGTH_LONG).show();
+        } else {
+            if (!btAdapter.isEnabled()) {
+                turnOnBt();
             }
             getPairedDevices();
+            startDiscovery();
+
         }
+
 
         return rootView;
 
     }
 
+    private void startDiscovery() {
+        mDeviceAdapter.clear();
+        btAdapter.cancelDiscovery();
+        btAdapter.startDiscovery();
+
+    }
+
+    private void turnOnBt() {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(receiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        getActivity().registerReceiver(receiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        getActivity().registerReceiver(receiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        getActivity().registerReceiver(receiver, filter);
+
+    }
+
     private void getPairedDevices() {
         devicesArray = btAdapter.getBondedDevices();
-        if (devicesArray.size()>0){
-            for(BluetoothDevice device:devicesArray){
-            mDeviceAdapter.add(device.getName()+"\n"+device.getAddress());
+        if (devicesArray.size() > 0) {
+            for (BluetoothDevice device : devicesArray) {
+                pairedDevices.add(device.getName());
             }
         }
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(receiver);
+    }
+
+
 }
